09ae1cb5c2b0696edb9d4cb117c863bd82ac38b6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-11-16 14:21:27 -0500

    trying to pass message from console to d3-panel

--- src/content-script/d3-panel-injected-into-page.js
+++ src/content-script/d3-panel-injected-into-page.js
@@ -6,6 +6,14 @@ port.onMessage.addListener(function (message) {
   console.log('message from d3-panel to content', message);
 });
 
+window.addEventListener("message", function(event) {
+  if (event.source != window)
+    return;
+
+  console.log('Message from window to d3-panel', event.data);
+  port.postMessage(event.data);
+}, false);
+
 document.d3Panel = function d3Panel() {
   console.log('d3Panel called');
 };
